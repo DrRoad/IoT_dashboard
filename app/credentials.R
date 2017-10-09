92f27c4bab9c64bfef4d691e133614254eeebfb5
Stephan Michard <stephan.michard@gmail.com>
2017-10-09 16:13:20 +0200

    version added

--- app/credentials.R
+++
@@ -1,6 +0,0 @@
-#mongo("ld_daten2", url = "mongodb://michas4:SsSPF8MU@clusterld-shard-00-00-uuoqh.mongodb.net:27017/db_ld?ssl=true&replicaSet=clusterLD-shard-0&authSource=admin")
-user_name <- "michas4"
-user_psw <- "SsSPF8MU"
-host <- "clusterld-shard-00-00-uuoqh.mongodb.net"
-port <- "27017"
-db_name <- "db_ld"
